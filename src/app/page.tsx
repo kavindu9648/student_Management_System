b1c7c7450587171bb2b951a4c6d880fcc3f60fe5
kavindu9648 <96469916+kavindu9648@users.noreply.github.com>
2025-04-09 15:21:43 +0530

    User table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+"use client";
+import { useEffect, useState, FormEvent } from "react";
+import { supabase } from "@/lib/supabase";
+import toast, { Toast } from "react-hot-toast";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
 export default function Home() {
   return (
     <>
+      {/*Student Management Form*/}
       <div className="container my-5">
         <h3 className="mb-4">Student Management</h3>
         <div className="row">
@@ -31,6 +38,38 @@ export default function Home() {
                   <button className="btn btn-primary w-100">Add</button>
                 </form>
               </div>
+            </div>
+          </div>
+          {/*Student Details Table*/}
+          <div className="col-md-8">
+            <div className="table-responsive">
+              <table className="table table-bordered">
+                <thead className="table-light">
+                  <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Gender</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Kavindu Eranga</td>
+                    <td>keranga297@gmail.com</td>
+                    <td>0766723151</td>
+                    <td>Male</td>
+                    <td>
+                      <button className="btn btn-warning btn-sm me-2">
+                        Edit
+                      </button>
+                      <button className="btn btn-danger btn-sm me-2">
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
