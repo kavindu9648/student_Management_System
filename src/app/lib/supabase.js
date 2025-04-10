14ac4f3845a614801366128a0126647564157c82
kavindu9648 <96469916+kavindu9648@users.noreply.github.com>
2025-04-10 15:12:23 +0530

    Connect to the Database Successfully

--- src/app/lib/supabase.js
+++ src/app/lib/supabase.js
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
+
 //replace with your Supabase URL and Anon Key
 const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY; 
